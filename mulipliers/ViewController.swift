f5ba95951449e56733714b90ca5ee35c779e33ad
Sateesh <sateesh.rachamadugu@letmecall.com>
2018-06-21 14:04:20 +0530

    Initial Commit

---
+++ mulipliers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mulipliers
+//
+//  Created by AktrixIOS01 on 21/06/18.
+//  Copyright © 2018 Aktrix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
